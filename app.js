e1da6751fd8064ca12c0554c6b65f6a0d20639d4
Jerzy Kozak <jkozak.pl@gmail.com>
2018-02-27 19:07:41 +0100

    New feature => Edit guest name added.

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const ul = document.getElementById('invitedList');
 
 function createLI(text) {
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
     const label = document.createElement('label');
     label.textContent = 'Confirmed';
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     label.appendChild(checkbox);
     li.appendChild(label);
-    const button = document.createElement('button');
-    button.textContent = 'Remove';
-    li.appendChild(button);
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    li.appendChild(editButton);
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    li.appendChild(removeButton);
     return li;
 }
 
@@ -39,8 +44,27 @@ ul.addEventListener('change', (e) => {
 
 ul.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-        const li = e.target.parentNode;
+        const button = e.target;
+        const li = button.parentNode;
         const ul = li.parentNode;
-        ul.removeChild(li);
+        if (button.textContent === 'Remove') {
+            ul.removeChild(li);
+        } else if (button.textContent === 'Edit') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'Save';
+
+        } else if (button.textContent === 'Save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'Edit';
+        }
     }
 });
